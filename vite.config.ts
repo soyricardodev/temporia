9dc2a4f278000c0f3febf59bd3ccca5350754a43
Ricardo Castro <soyricardodev@proton.me>
2025-03-26 13:03:23 -0400

    add vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+import { PWAConfig } from "./src/lib/pwa-config"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA(PWAConfig)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
